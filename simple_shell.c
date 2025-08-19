f43e1d327e12d0d8dc645f231916098ca4e27ee6
Kenan <yahyayevkenan57@gmail.com>
2025-08-19 19:14:13 +0400

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -2,65 +2,71 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_ARGS 64
+#define MAX_CMD_LEN 1024
+#define MAX_ARGS 100
 
+/**
+ * main - simple shell
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
+	char cmd[MAX_CMD_LEN];
+	char *argv[MAX_ARGS];
+	pid_t pid;
+	int status, i;
 
-    while (1)
-    {
-        printf("#cisfun$ ");
-        fflush(stdout);
+	while (1)
+	{
+		write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            perror("getline");
-            break;
-        }
+		if (fgets(cmd, sizeof(cmd), stdin) == NULL)
+		{
+			write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
 
-        if (line[nread - 1] == '\n')
-            line[nread - 1] = '\0';
+		/* Remove newline character */
+		cmd[strcspn(cmd, "\n")] = '\0';
 
-        char *argv[MAX_ARGS];
-        int i = 0;
-        char *token = strtok(line, " ");
+		/* Tokenize command */
+		i = 0;
+		argv[i] = strtok(cmd, " ");
+		while (argv[i] != NULL && i < MAX_ARGS - 1)
+		{
+			i++;
+			argv[i] = strtok(NULL, " ");
+		}
+		argv[i] = NULL;
 
-        while (token != NULL && i < MAX_ARGS - 1)
-        {
-            argv[i++] = token;
-            token = strtok(NULL, " ");
-        }
-        argv[i] = NULL;
+		/* Skip empty input */
+		if (argv[0] == NULL)
+			continue;
 
-        if (argv[0] == NULL)
-            continue;
+		pid = fork();
+		if (pid == 0)
+		{
+			/* Child process */
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if (pid > 0)
+		{
+			/* Parent process */
+			wait(&status);
+		}
+		else
+		{
+			perror("fork");
+		}
+	}
 
-        pid_t pid = fork();
-
-        if (pid == -1)
-        {
-            perror("fork");
-            continue;
-        }
-        else if (pid == 0)
-        {
-            execve(argv[0], argv, NULL);
-            perror("execve");
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            int status;
-            wait(&status);
-        }
-    }
-
-    free(line);
-    return 0;
+	return (0);
 }
