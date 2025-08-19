7818a9ccc03393627ce90c93970faab845cee294
Kenan <yahyayevkenan57@gmail.com>
2025-08-19 18:27:53 +0400

    Create simple_shell.c

---
+++ simple_shell.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the simple shell
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("Simple Shell started\n");
+    return (0);
+}
