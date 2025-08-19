534b4dcd366d09737df2572c69dc53654cef1633
Kenan <yahyayevkenan57@gmail.com>
2025-08-19 19:10:29 +0400

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -2,70 +2,56 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
-#define BUFFER_SIZE 1024
-#define DELIM " \t\r\n\a"
+#define MAX_INPUT 1024
+#define MAX_ARGS 64
 
-/**
- * main - Entry point for a simple shell.
- * Return: 0 on success.
- */
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	char *token;
-	char *argv[BUFFER_SIZE];
-	pid_t pid;
-	int status;
-	int i;
+    char input[MAX_INPUT];
+    char *args[MAX_ARGS];
+    char *token;
+    pid_t pid;
+    int status;
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "#cisfun$ ", 9);
+    while (1)
+    {
+        printf("#cisfun$ ");
+        if (!fgets(input, MAX_INPUT, stdin))
+        {
+            printf("\n");
+            break;
+        }
 
-		nread = getline(&line, &len, stdin);
-		if (nread == -1)
-			break;
+        input[strcspn(input, "\n")] = '\0';  // Remove newline
 
-		token = strtok(line, "\n");
-		while (token != NULL)
-		{
-			char *subtoken = strtok(token, DELIM);
-			i = 0;
+        int i = 0;
+        token = strtok(input, " \t");
+        while (token && i < MAX_ARGS - 1)
+        {
+            args[i++] = token;
+            token = strtok(NULL, " \t");
+        }
+        args[i] = NULL;
 
-			while (subtoken != NULL && i < BUFFER_SIZE - 1)
-			{
-				argv[i++] = subtoken;
-				subtoken = strtok(NULL, DELIM);
-			}
-			argv[i] = NULL;
+        if (args[0] == NULL)
+            continue;
 
-			if (argv[0] != NULL)
-			{
-				pid = fork();
-				if (pid == 0)
-				{
-					if (execvp(argv[0], argv) == -1)
-						perror(argv[0]);
-					exit(EXIT_FAILURE);
-				}
-				else if (pid < 0)
-				{
-					perror("fork");
-				}
-				else
-				{
-					wait(&status);
-				}
-			}
-			token = strtok(NULL, "\n");
-		}
-	}
-	free(line);
-	return (0);
+        pid = fork();
+        if (pid == 0)
+        {
+            execvp(args[0], args);
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid > 0)
+        {
+            wait(&status);
+        }
+        else
+        {
+            perror("fork");
+        }
+    }
+    return 0;
 }
