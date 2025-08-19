cb3925fd29c2cc5032b2211332013fbd1b81c75e
Kenan <yahyayevkenan57@gmail.com>
2025-08-19 19:02:47 +0400

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -4,95 +4,62 @@
 #include <string.h>
 #include <sys/wait.h>
 
-#define MAX_ARGS 10
 extern char **environ;
 
-char *trim_spaces(char *str)
-{
-    char *end;
-
-    while (*str == ' ' || *str == '\t')
-        str++;
-
-    if (*str == 0)
-        return str;
-
-    end = str + strlen(str) - 1;
-    while (end > str && (*end == ' ' || *end == '\t' || *end == '\n'))
-        end--;
-
-    *(end + 1) = '\0';
-
-    return str;
-}
-
-int split_line(char *line, char **args)
-{
-    int i = 0;
-    char *token;
-
-    token = strtok(line, " \t\r\n");
-    while (token != NULL && i < MAX_ARGS - 1)
-    {
-        args[i++] = token;
-        token = strtok(NULL, " \t\r\n");
-    }
-    args[i] = NULL;
-    return i;
-}
-
+/**
+ * main - Entry point for simple shell 0.1
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    pid_t pid;
-    char *args[MAX_ARGS];
-    int argc;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	pid_t pid;
+	char *args[2];
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            printf("#cisfun$ ");
+	while (1)
+	{
+		printf("#cisfun$ ");
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			printf("\n");
+			break;
+		}
 
-        read = getline(&line, &len, stdin);
-        if (read == -1)
-        {
-            if (isatty(STDIN_FILENO))
-                printf("\n");
-            break;
-        }
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
 
-        char *cmd = trim_spaces(line);
-        if (cmd[0] == '\0')
-            continue;
+		if (line[0] == '\0')
+			continue;
 
-        argc = split_line(cmd, args);
-        if (argc == 0)
-            continue;
+		args[0] = line;
+		args[1] = NULL;
 
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("fork");
-            free(line);
-            exit(EXIT_FAILURE);
-        }
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			free(line);
+			exit(EXIT_FAILURE);
+		}
 
-        if (pid == 0)
-        {
-            if (execve(args[0], args, environ) == -1)
-            {
-                perror("./shell");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            wait(NULL);
-        }
-    }
+		if (pid == 0)
+		{
+			if (execve(args[0], args, environ) == -1)
+			{
+				perror("./hsh");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
 
-    free(line);
-    return (0);
+	free(line);
+	return (0);
 }
