b0f3884bea23f995bcf470b2188532a5902a637c
Kenan <yahyayevkenan57@gmail.com>
2025-08-21 17:30:49 +0400

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+char *resolve_path(char *cmd);
+void tokenize_input(char *line, char **argv);
+
+#endif
