71f25c910eb74a682f6d801dfbbec8db71d94942
Kenan <yahyayevkenan57@gmail.com>
2025-08-26 17:53:08 +0400

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,14 +1,15 @@
 #include "shell.h"
 
 /**
- * execute - Handle command execution with fork + execve
- * @args: argument list
- * @argv: program name for error messages
+ * execute - Executes a command if valid
+ * @args: command args
+ * @program_name: program name (argv[0])
  */
-void execute(char **args, char *program_name);
+void execute(char **args, char *program_name)
 {
 	pid_t pid;
-	char *cmd = args[0], *path = NULL;
+	char *cmd = args[0];
+	char *path = NULL;
 
 	if (cmd[0] == '/' || cmd[0] == '.')
 	{
@@ -22,7 +23,7 @@ void execute(char **args, char *program_name);
 
 	if (!path)
 	{
-		fprintf(stderr, "%s: 1: %s: not found\n", argv[0], cmd);
+		fprintf(stderr, "%s: 1: %s: not found\n", program_name, cmd);
 		return;
 	}
 
@@ -30,9 +31,13 @@ void execute(char **args, char *program_name);
 	if (pid == 0)
 	{
 		if (execve(path, args, environ) == -1)
+		{
 			perror("execve");
-		exit(EXIT_FAILURE);
+			exit(EXIT_FAILURE);
+		}
 	}
 	else if (pid > 0)
+	{
 		wait(NULL);
+	}
 }
