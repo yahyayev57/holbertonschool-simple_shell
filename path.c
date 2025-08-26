d6a4aeb2e798e260ec928b812faebf8b99d833ec
Kenan <yahyayevkenan57@gmail.com>
2025-08-26 17:53:52 +0400

    Update path.c

--- path.c
+++ path.c
@@ -7,20 +7,25 @@
  */
 char *find_path(char *cmd)
 {
-	char *path = getenv("PATH");
-	char *token, *full;
+	char *path_env = getenv("PATH");
+	char *token;
 	static char buffer[1024];
 
-	if (!path)
+	if (!path_env)
 		return (NULL);
 
-	token = strtok(path, ":");
+	path_env = strdup(path_env); /* avoid strtok modifying env var */
+	token = strtok(path_env, ":");
 	while (token)
 	{
 		snprintf(buffer, sizeof(buffer), "%s/%s", token, cmd);
 		if (access(buffer, X_OK) == 0)
+		{
+			free(path_env);
 			return (buffer);
+		}
 		token = strtok(NULL, ":");
 	}
+	free(path_env);
 	return (NULL);
 }
